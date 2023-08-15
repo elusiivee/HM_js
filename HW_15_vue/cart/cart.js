06238758da12d98c8b54c08336f44e8e3484d5dd
elusiivee <andrii.1669@gmail.com>
2023-08-15 11:32:09 +0200

    fixed some issues

--- HW_15_vue/cart/cart.js
+++ HW_15_vue/cart/cart.js
@@ -5,20 +5,40 @@ const app = Vue.createApp({
         incart:0,
         price:100,
         like:19344,
+        userslike:0,
+        total: 0
     }},
     methods:{
         toCart(){
             if (this.instock>0){
                 this.instock--;
                 this.incart++;
+                this.total+=this.price;
             }
         },
         fromCart(){
             if (this.incart>0){
                 this.instock++;
                 this.incart--;
+                this.total-=this.price;
             }
         },
-    }
+        like(){                                         // added funtion to put like
+            if(this.userslike !== 1){
+                this.like++;
+                this.userslike++;
+            }
+            
+        },  
+        dislike(){                                      // added funtion to put dislike
+            if(this.userslike !== 0){
+                this.like--;
+                this.userslike--;
+
+            }
+            
+        },
+        }
+    
 });
 app.mount('#app')
